5a1574a0799d9d6963135fcbe43884dd47d793cc
elliechen <elliechen@lulainno.com>
2024-01-11 10:37:33 +0800

    add : Arguments.js 解答

--- Arguments.js
+++ Arguments.js
@@ -1,1 +1,15 @@
-123
+// 3. ** Arguments使用：**
+//   - 寫一個函式`sum`，接受任意數量的參數，並計算它們的總和。使用`arguments`物件。
+
+function sum(...arguments) {
+  let total = 0
+  arguments.forEach(function (item) {
+    total += item
+  })
+  return total
+}
+
+console.log(sum(1, 2, 3, 5))
+
+
+// node Arguments.js
